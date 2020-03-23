acbdaa9ef5335e2a77cadc57bbe6bc96aa7ca127
pluto <ai_liucy@163.com>
2020-03-23 23:48:20 +0800

    3.无重复字符的最长子串，更新

--- 3.无重复字符的最长子串.py
+++ 3.无重复字符的最长子串.py
@@ -20,30 +20,46 @@
 
 
 class Solution:
+
     def lengthOfLongestSubstring(self, s: str) -> int:
         # 字符串为空则返回零
         if not s:
             return 0
 
+        length_str = []  # 所有的最长子串
         window = []     # 滑动窗口数组
-        max_length = 0  # 最长串长度
+        max_length = 0  # 最长子串的长度
 
         # 遍历字符串
         for c in s:
+
             # 如果字符不在滑动窗口中，则直接扩展窗口
             if c not in window:
                 # 使用当前字符扩展窗口
                 window.append(c)
-            # 如果字符在滑动窗口中，则
-            # 1. 从窗口中移除重复字符及之前的字符串部分
-            # 2. 再扩展窗口
+            # 如果字符在滑动窗口中
             else:
-                # 从窗口中移除重复字符及之前的字符串部分，新字符串即为无重复字符的字符串
+                # 1 .从窗口中移除重复字符及之前的字符串部分，新字符串即为无重复字符的字符串
                 window[:] = window[window.index(c) + 1:]
-                # 扩展窗口
+                # 2 .扩展窗口
                 window.append(c)
 
             # 更新最大长度
             max_length = max(len(window), max_length)
+            if max_length <= len(window) and (window not in length_str):
+                if ''.join(window) not in length_str:
+                    length_str.append(''.join(window))
 
+        all_length_str = [i for i in length_str if len(i) == max_length]
+        print(all_length_str)
         return max_length if max_length != 0 else len(s)
+
+
+def main():
+    str_a = "pwwekw"
+    a = Solution()
+    print(a.lengthOfLongestSubstring(str_a))
+
+
+if __name__ == '__main__':
+    main()
