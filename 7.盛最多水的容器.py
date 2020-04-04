36939546f3a2dfed54e54b46e581cfd722d377b0
pluto <ai_liucy@163.com>
2020-04-04 21:56:18 +0800

    双指针法

--- 7.盛最多水的容器.py
+++ 7.盛最多水的容器.py
@@ -14,13 +14,13 @@ from typing import List
 
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        i, j, res = 0, len(height) - 1, 0
+        i, j, res = 0, len(height)-1, 0
         while i < j:
             if height[i] < height[j]:
-                res = max(res, height[i] * (j - i))
+                res = max(res, height[i]*(j-i))
                 i += 1
             else:
-                res = max(res, height[j] * (j - i))
+                res = max(res, height[j]*(j-i))
                 j -= 1
         return res
 
